73775e31367bba70815d794bc4212a182210f545
jmascena <joao167pedro@gmail.com>
2024-02-23 22:22:53 -0300

    Add backend files and configurations

---
+++ backend/src/main/java/com/air/airbackend/AirbackendApplication.java
@@ -0,0 +1,13 @@
+package com.air.airbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirbackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/air/airbackend/AirbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.air.airbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
