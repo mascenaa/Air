9662c95e8c0b146071136f1b0287fc0f83b3c004
Murakami1410 <felipe.murakami10@gmail.com>
2024-03-26 09:46:38 -0300

    Update login page UI

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -11,16 +11,16 @@ export default function Login() {
             <div>
                 <h1 className="text-4xl font-bold p-10">AIR</h1>
             
-                <Button className="bg-white font-black text-black rounded-2xl w-1/4 hover:bg-gray-200">Entrar com Gmail</Button>
+                <Button className="bg-white font-black text-black rounded-2xl w-1/4 hover:bg-gray-200">Login with Gmail</Button>
             </div>
             <div>
             <div className="mx-auto p-10 w-2/4">
                 <p className="text-left">Email</p>
                 <Input placeholder="" className="bg-white" />
-                <p className="text-left">Senha</p>
+                <p className="text-left">Password</p>
                 <Input placeholder="" className="bg-white"/>
-                <p className="text-right ">Esqueceu a senha?</p>
-                <Button className="bg-white font-bold text-black rounded-2xl w-full hover:bg-gray-200 mt-10 ">Entrar</Button>
+                <p className="text-right ">Forget Password?</p>
+                <Button className="bg-white font-bold text-black rounded-2xl w-full hover:bg-gray-200 mt-10 ">Login</Button>
             </div>
             </div>
 
